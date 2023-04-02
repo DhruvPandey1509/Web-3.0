429fb5ce391b958dfbb67408ec4fcfc92cc9be72
Dhruv Pandey <dhruvpandey1503@gmail.com>
2023-04-02 16:28:21 +0530

    web3.0 project initialization

---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+import Welcome from "./Welcome";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Services from "./Services";
+import Transactions from "./Transactions";
+import Footer from "./Footer";
+
+export {
+    Welcome, Loader, Navbar, Footer, Services, Transactions
+}
